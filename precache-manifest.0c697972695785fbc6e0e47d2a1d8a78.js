5592133743b57803d966939aa77d507390b2d0b4
Jass77 <68679767+Jass77@users.noreply.github.com>
2020-07-27 17:37:53 +1000

    Updates

---
+++ precache-manifest.0c697972695785fbc6e0e47d2a1d8a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ed0cc543067661b33ad9ac7c6733ce",
+    "url": "/mydemoapp/index.html"
+  },
+  {
+    "revision": "a6b7c386a2f03067b002",
+    "url": "/mydemoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f535f12276d5419e827b",
+    "url": "/mydemoapp/static/js/2.b0080179.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mydemoapp/static/js/2.b0080179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b7c386a2f03067b002",
+    "url": "/mydemoapp/static/js/main.94cb558c.chunk.js"
+  },
+  {
+    "revision": "2fb4b8de10334af95fc4",
+    "url": "/mydemoapp/static/js/runtime-main.f1ca396d.js"
+  }
+]);
